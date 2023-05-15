318b84e92321ec45326d474fd29416eca21c54e7
Lucas Kranz <kranz12@gmail.com>
2023-05-15 08:41:18 -0300

    Add project files.

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,88 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto: Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            CategoriaId = categoriaId;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+
+            Validar();
+        }
+
+        public void Ativar() => Ativo = true;
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do Produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do Produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do Produto não pode estar vazio");
+            Validacoes.ValidarSeDiferente(CategoriaId, Guid.Empty, "O campo CategoriaId do Produto não pode estar vazio");
+        }
+    }
+
+    public class Categoria : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+    }
+}
